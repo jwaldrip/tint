ee9f6cbef218247c726944e4db132eb6fd20bcc6
Jason Waldrip <jason@waldrip.net>
2014-10-01 22:32:07 -0600

    everything in render

--- type-Style.go
+++ type-Style.go
@@ -9,58 +9,63 @@ type Style struct {
 	Color      int
 	Background int
 	Bold       bool
+	Dim        bool
 	Blink      bool
 	Underline  bool
 	Inverted   bool
 	Use256     bool
 }
 
-// Render renders a style on a string
+// Render the styled string
 func (s *Style) Render(str string) string {
-	return strings.Join([]string{s.escapeSequence(), str, s.resetSequence()}, "")
-}
-
-func (s *Style) getColor() string {
-	clr := s.Color
-	if clr == 0 && !s.Use256 {
-		clr = DefaultColor
-	}
-	return strconv.Itoa(clr)
-}
-
-func (s *Style) getBackground() string {
-	bg := s.Background
-	if bg == 0 && !s.Use256 {
-		bg = DefaultColor
-	}
-	return strconv.Itoa(bg + 10)
-}
-
-func (s *Style) resetSequence() string {
-	return fmt.Sprintf("%s[%sm", escape, strconv.Itoa(reset))
-}
-
-func (s *Style) escapeSequence() string {
 	var strs []string
 	if s.Bold {
 		strs = append(strs, strconv.Itoa(bold))
 	}
-	if s.Blink {
-		strs = append(strs, strconv.Itoa(blink))
+	if s.Dim {
+		strs = append(strs, strconv.Itoa(dim))
 	}
 	if s.Underline {
 		strs = append(strs, strconv.Itoa(underline))
+	}
+	if s.Blink {
+		strs = append(strs, strconv.Itoa(blink))
 	}
 	if s.Inverted {
 		strs = append(strs, strconv.Itoa(invert))
 	}
 	if s.Use256 {
-		strs = append(strs, "38;5")
-	}
-	strs = append(strs, s.getColor())
-	if s.Use256 {
 		strs = append(strs, "48;5")
+		strs = append(strs, s.getColor())
+		strs = append(strs, "38;5")
+		strs = append(strs, s.getBackground())
+	} else {
+		if s.Color != 0 && s.Color != DefaultColor {
+			strs = append(strs, s.getColor())
+		}
+		if s.Background != 0 && s.Background != DefaultColor {
+			strs = append(strs, s.getBackground())
+		}
 	}
-	strs = append(strs, s.getBackground())
-	return fmt.Sprintf("%s[%sm", escape, strings.Join(strs, ";"))
+	if len(strs) == 0 {
+		return str
+	}
+	return fmt.Sprintf(
+		"%s[%sm%s%s[%sm",
+		escape,
+		strings.Join(strs, ";"),
+		str,
+		escape,
+		strconv.Itoa(reset),
+	)
+}
+
+func (s *Style) getColor() string {
+	clr := s.Color
+	return strconv.Itoa(clr)
+}
+
+func (s *Style) getBackground() string {
+	bg := s.Background
+	return strconv.Itoa(bg + 10)
 }
